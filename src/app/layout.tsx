f7ded783df54d32dff84bdbfd20d258ad03a483b
Runterya <97337724+Runteryaa@users.noreply.github.com>
2026-01-30 22:59:21 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,10 @@ export default function RootLayout({
       >
         {children}
 
-        </Script>
+        <div id="frame" style="width: 100%;margin: auto;position: relative; z-index: 99998;">
+          <iframe data-aa='2425973' src='//acceptable.a-ads.com/2425973/?size=Adaptive'
+                            style='border:0; padding:0; width:70%; height:auto; overflow:hidden;display: block;margin: auto'></iframe>
+        </div>
       </body>
     </html>
   );
